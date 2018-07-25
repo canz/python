772410d38a93b22b80bf121ca8cbf93d2c0b4fdd
Stephen Carins <stephen@carins.com.au>
2018-07-25 15:33:31 +1000

    fix

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import yaml
 for i in range(5):
     print(random.random())
 
-with open("C:\Users\steph\github\python\settings.yaml", 'r') as stream:
+with open("C:\\Users\\steph\\github\\python\\settings.yaml", 'r') as stream:
     try:
         print(yaml.load(stream))
     except yaml.YAMLError as exc:
