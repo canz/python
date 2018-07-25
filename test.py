5fa7ae6f90ad81c4df56b8c70eb9eb86d0fad4aa
Stephen Carins <stephen@carins.com.au>
2018-07-25 16:21:08 +1000

    sql

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import random
 import yaml
+import pymysql
+
 
 for i in range(5):
     print(random.random())
@@ -9,3 +11,28 @@ with open("C:\\Users\\steph\\github\\python\\settings.yaml", 'r') as stream:
         print(yaml.load(stream))
     except yaml.YAMLError as exc:
         print(exc)
+
+# Connect
+db = pymysql.connect(host="192.168.10.240",
+                     user="testuser",
+                     passwd="demopass",
+                     db="pythontest")
+cursor = db.cursor()
+
+# Execute SQL select statement
+cursor.execute("SELECT * FROM user_details")
+
+# Commit your changes if writing
+# In this case, we are only reading data
+# db.commit()
+
+# Get the number of rows in the resultset
+numrows = cursor.rowcount
+
+# Get and display one row at a time
+for x in range(0, numrows):
+    row = cursor.fetchone()
+    print (row[0], "-->", row[1])
+
+# Close the connection
+db.close()
