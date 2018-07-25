28cb69b0f887ce6b0daeb5125e702e90e139a5da
Stephen Carins <stephen@carins.com.au>
2018-07-25 12:27:18 +1000

    Init

---
+++ test.py
@@ -0,0 +1,12 @@
+import random
+import yaml
+
+for i in range(5):
+    print(random.random())
+
+
+with open("D:\python\settings.yaml", 'r') as stream:
+    try:
+        print(yaml.load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
